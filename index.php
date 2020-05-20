8fd8ddb8dceea732dd492388f8be4e6e8e2dff2f
Salvatore Lauriola <salvatore6324@gmail.com>
2020-05-20 16:53:01 +0200

    exercise done with css.

--- index.php
+++ index.php
@@ -9,30 +9,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="dist/scss/main.scss">
+    <link rel="stylesheet" href="dist/scss/main.css">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet">
+
 </head>
 <body>
+<header>
+    <div class="container_head">
+        <img src="dist/img/spotify-logo.png" alt="logo">
+    </div>
+</header>
     
 
+<main class="music">
+    <div class="container">
+        <div class="wrapper">
+        <?php foreach($album as $data) {?>
+            <div class="fixing_ul">
+                <ul>
+                <li><img src="<?php echo $data['poster']?>" alt="image"> </li>
+                <li> <h3><?php echo $data['title']?></h3>  </li>
+                <li> <h4><?php echo $data['author']?></h4> </li>
+                <li> <h6><?php echo $data['year']?></h6> </li>
+                </ul>
+            </div>
+        <?php } ?>
+        </div>
+        
+        
+    
+    </div>
+</main>
 
-<ul>
-<?php foreach($album as $data) {?>
 
-    <li><img src="<?php echo $data['poster']?>" alt="image"> </li>
-    <li> <?php echo $data['title']?> </li>
-    <li> <?php echo $data['author']?> </li>
-    <li> <?php echo $data['year']?> </li>
-<?php } ?>
-</ul>
-
-
-
-
-
-
-
-
-
-    <script src="dist/js/main.js"></script>
+<script src="dist/js/main.js"></script>
 </body>
 </html>
