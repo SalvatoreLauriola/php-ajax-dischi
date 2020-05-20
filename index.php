fa98aa9228f340fa5f2365e629fa0fbdaed76df8
Salvatore Lauriola <salvatore6324@gmail.com>
2020-05-20 13:48:01 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="dist/scss/main.scss">
+</head>
+<body>
+    
+
+
+
+
+
+
+
+
+
+
+
+    <script src="dist/js/main.js"></script>
+</body>
+</html>
